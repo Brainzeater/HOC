ed38256ff684ca11120a1fef1cbd3803bbc78597
Alexander Kushnarev <brainzeater@gmail.com>
2019-12-30 17:12:58 +0300

    A* algortihm
    - Scripts to implement a basic version of A*
    - Cell Prefab to highlight grid nodes and the calculated path

---
+++ HeroesOfCode/Assets/Scripts/Grid.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class Grid : MonoBehaviour
+{
+    public float nodeRadius;
+    private float nodeDiameter;
+
+    public int gridSizeX, gridSizeY;
+
+
+    //    public Transform player;
+    public LayerMask unwalkableMask;
+    private Vector2 gridWorldSize;
+    public GameObject cell;
+    private GameObject cellInstance;
+    private Node[,] grid;
+    private Node lastHighlightedNode;
+    [HideInInspector] public List<Node> path;
+
+
+    void Start()
+    {
+//        nodeDiameter = node.transform.localScale.x;
+        nodeDiameter = nodeRadius * 2;
+        //        nodeDiameter = nodeSprite.transform.localScale.x;
+        //        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        //        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        gridWorldSize.x = gridSizeX * nodeDiameter;
+        gridWorldSize.y = gridSizeY * nodeDiameter;
+        GetComponent<BoxCollider2D>().size = gridWorldSize;
+        Debug.Log(gridWorldSize);
+        CreateGrid();
+    }
+
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 worldBottomLeft =
+            transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.up * gridWorldSize.y / 2;
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) +
+                                     Vector3.up * (y * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
+//                bool walkable = !(Physics.CheckBox(worldPoint, new Vector3(nodeRadius, nodeRadius, nodeRadius),
+//                    Quaternion.identity, unwalkableMask));
+                grid[x, y] = new Node(walkable, worldPoint, x, y);
+            }
+        }
+    }
+
+    void OnMouseOver()
+    {
+        Vector3 mousePosition = GetMousePosition();
+        if (!cellInstance)
+        {
+            lastHighlightedNode = NodeFromWorldPoint(mousePosition);
+            cellInstance = Instantiate(cell,
+                new Vector3(lastHighlightedNode.worldPosition.x, lastHighlightedNode.worldPosition.y, 0),
+                Quaternion.identity);
+
+            cellInstance.transform.parent = gameObject.transform;
+        }
+        else
+        {
+            Node currentNode = NodeFromWorldPoint(mousePosition);
+            if (!lastHighlightedNode.Equals(currentNode))
+            {
+                Destroy(cellInstance);
+            }
+        }
+    }
+
+    public Vector3 GetMousePosition()
+    {
+        var v3 = Input.mousePosition;
+        v3.z = transform.position.z;
+        return Camera.main.ScreenToWorldPoint(v3);
+    }
+
+    void OnMouseExit()
+    {
+        if (cellInstance)
+        {
+            Destroy(cellInstance);
+        }
+    }
+
+    public List<Node> GetNeighbours(Node node)
+    {
+        List<Node> neighbours = new List<Node>();
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x == 0 && y == 0)
+                    continue;
+                int checkX = node.gridX + x;
+                int checkY = node.gridY + y;
+
+                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+                {
+                    neighbours.Add(grid[checkX, checkY]);
+                }
+            }
+        }
+
+        return neighbours;
+    }
+
+    public Node NodeFromWorldPoint(Vector3 worldPosition)
+    {
+        float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+        float percentY = (worldPosition.y + gridWorldSize.y / 2) / gridWorldSize.y;
+
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
+
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+
+        return grid[x, y];
+    }
+
+//    void OnDrawGizmos()
+//    {
+//        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, gridWorldSize.y, 1));
+//        if (grid != null)
+//        {
+////            Node playerNode = NodeFromWorldPoint(player.position);
+//            foreach (Node n in grid)
+//            {
+//                Gizmos.color = (n.walkable) ? Color.white : Color.red;
+////                if (playerNode == n)
+////                {
+////                    Gizmos.color = Color.magenta;
+////                }
+//
+//                if (path != null)
+//                {
+//                    if (path.Contains(n))
+//                    {
+//                        Gizmos.color = Color.black;
+//                    }
+//                }
+//                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .01f));
+//            }
+//        }
+//    }
+
+    public void HighlightPath()
+    {
+        foreach (Node node in path)
+        {
+            GameObject pathCell = Instantiate(cell, new Vector3(node.worldPosition.x, node.worldPosition.y, 0),
+                Quaternion.identity);
+
+            pathCell.transform.parent = gameObject.transform;
+        }
+    }
+}
---
+++ HeroesOfCode/Assets/Scripts/Node.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public Vector3 worldPosition;
+    public int gridX, gridY;
+    public bool walkable;
+
+    public int gCost, hCost;
+    public Node parent;
+
+    private SpriteRenderer renderer;
+    private Color highlightColor = Color.yellow;
+    private Color defaultColor = new Color(1, 1, 0, .2f);
+
+    public Color Color { get; set; }
+
+
+    public Node(bool walkable, Vector3 worldPosition, int gridX, int gridY)
+    {
+        this.walkable = walkable;
+        this.worldPosition = worldPosition;
+        this.gridX = gridX;
+        this.gridY = gridY;
+        Color = defaultColor;
+    }
+
+    public int fCost
+    {
+        get { return gCost + hCost; }
+    }
+
+    void ChangeColor()
+    {
+    }
+
+    public override bool Equals(object obj)
+    {
+        Node other = obj as Node;
+        if (other == null) return false;
+        return (this.gridX == other.gridX) && (this.gridY == other.gridY);
+    }
+}
