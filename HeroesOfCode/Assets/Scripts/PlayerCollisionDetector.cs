a6b1e94b4d4f387f5a21e6b3a4c4b7a70614fc1c
Brainzeater <brainzeater@gmail.com>
2020-01-14 06:05:21 +0300

    Battle Trigger
    - Approaching the Enemy Army triggers the Battle Scene
    - Empty Battle Scene as a placeholder
    - Scene Loader Script
    - Collision Detector Script

---
+++ HeroesOfCode/Assets/Scripts/PlayerCollisionDetector.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionDetector : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneLoader.LoadBattleScene();
+    }
+}
---
+++ HeroesOfCode/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public static void LoadBattleScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
