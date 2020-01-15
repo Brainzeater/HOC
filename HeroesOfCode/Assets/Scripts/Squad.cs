4edcca6a0ae83bcd91103226fa5b1af25ac523c0
Brainzeater <brainzeater@gmail.com>
2020-01-15 21:29:55 +0300

    Squad Code Structure Update
    - Enemy Squad Prefab
    - Player and Enemy Squads inherit from Squad Script

--- HeroesOfCode/Assets/Scripts/Squad.cs
+++ HeroesOfCode/Assets/Scripts/Squad.cs
@@ -6,28 +6,24 @@ using UnityEngine;
 public class Squad : MonoBehaviour
 {
     public Unit unit;
-    public int numOfUnits;
+    public int numberOfUnits;
 
     private int DealingDamage { get; set; }
 //    private int ReceivedDamage { get; set; }
     private int HP { get; set; }
 
-    public TMP_Text numOfUnitsText;
-    public TMP_Text unitHPText;
-    public TMP_Text unitDamageText;
+    public TMP_Text numberOfUnitsText;
 
 //    private bool usedActiveSkill;
 
-    void Start()
+    public virtual void Start()
     {
-        numOfUnitsText.text = numOfUnits.ToString();
-        unitHPText.text = unit.hp.ToString();
-        unitDamageText.text = unit.damage.ToString();
+        numberOfUnitsText.text = numberOfUnits.ToString();
     }
 
     void CalculateDealingDamage()
     {
-        DealingDamage = numOfUnits * unit.damage;
+        DealingDamage = numberOfUnits * unit.damage;
     }
 
     void ReceiveDamage(int damage)
@@ -40,13 +36,13 @@ public class Squad : MonoBehaviour
         else
         {
             int killedUnits = damage / unit.hp;
-            numOfUnits -= killedUnits;
+            numberOfUnits -= killedUnits;
         }
     }
 
     void CalculateSquadHP()
     {
-        HP = numOfUnits * unit.hp;
+        HP = numberOfUnits * unit.hp;
     }
 
     void CalculateNumberOfUnitsFromHP()
