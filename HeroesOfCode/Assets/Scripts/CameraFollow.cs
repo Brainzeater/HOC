3ace31929daa70546b295258fbd5cf2c96a2105b
Brainzeater <brainzeater@gmail.com>
2020-01-21 20:40:16 +0300

    Camera Bounds Fix
    Now Camera Script limits camera movement inside the bounds.

--- HeroesOfCode/Assets/Scripts/CameraFollow.cs
+++ HeroesOfCode/Assets/Scripts/CameraFollow.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -10,9 +11,18 @@ public class CameraFollow : MonoBehaviour
     public float smoothSpeed = 7f;
     public Vector3 offset;
 
+    public float xAxisMin;
+    public float xAxisMax;
+    public float yAxisMin;
+    public float yAxisMax;
+
     void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
+        Vector3 boundedPosition = new Vector3(
+            Mathf.Clamp(target.position.x, xAxisMin, xAxisMax),
+            Mathf.Clamp(target.position.y, yAxisMin, yAxisMax),
+            0);
+        Vector3 desiredPosition = boundedPosition + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
 
         transform.position = smoothedPosition;
