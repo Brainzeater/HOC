4497ab417fa7266dbe43f75ecdce1939eca8e203
Brainzeater <brainzeater@gmail.com>
2020-01-27 00:20:22 +0300

    Player Animation
    - Animation speed depends on the surface
    - Animation for 4 (8) directions
    - Idle animaiton

--- HeroesOfCode/Assets/Scripts/Movement.cs
+++ HeroesOfCode/Assets/Scripts/Movement.cs
@@ -20,6 +20,14 @@ public class Movement : MonoBehaviour
 
     private const float radius = 0.01f;
 
+    private Animator animator;
+    private float animationSpeedModifier;
+
+    void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
     // Starts a coroutine that moves the player object.
     public void BeginMovement(Vector3[] path)
     {
@@ -36,6 +44,7 @@ public class Movement : MonoBehaviour
     IEnumerator FollowPath()
     {
         Vector3 currentWaypoint = Path[0];
+        Vector3 direction;
         while (true)
         {
             if (transform.position == currentWaypoint)
@@ -43,6 +52,8 @@ public class Movement : MonoBehaviour
                 targetIndex++;
                 if (targetIndex >= Path.Length)
                 {
+                    animator.SetFloat("HorizontalSpeed", 0f);
+                    animator.SetFloat("VerticalSpeed", 0f);
                     GameEvents.current.MovementFinished();
                     yield break;
                 }
@@ -50,8 +61,15 @@ public class Movement : MonoBehaviour
                 currentWaypoint = Path[targetIndex];
             }
 
+//            print(currentWaypoint - transform.position);
+            direction = (currentWaypoint - transform.position);
+            print($"x: {detectDirection(direction.x)} y: {detectDirection(direction.y)}");
+            animator.SetFloat("HorizontalSpeed", detectDirection(direction.x) - animationSpeedModifier);
+            animator.SetFloat("VerticalSpeed", detectDirection(direction.y) - animationSpeedModifier);
             transform.position =
                 Vector3.MoveTowards(transform.position, currentWaypoint, speed * Time.deltaTime);
+
+//            print(transform.position);
             //TODO: Destroy past cell
             yield return null;
         }
@@ -60,6 +78,7 @@ public class Movement : MonoBehaviour
     void Start()
     {
         speed = defaultSpeed;
+        animationSpeedModifier = 0.25f;
     }
 
     void Update()
@@ -67,14 +86,33 @@ public class Movement : MonoBehaviour
         if (Physics2D.OverlapCircle(gameObject.transform.position, radius, roadMask))
         {
             speed = increasedSpeed;
+            animationSpeedModifier = 0f;
         }
-        else if(Physics2D.OverlapCircle(gameObject.transform.position, radius, iceMask))
+        else if (Physics2D.OverlapCircle(gameObject.transform.position, radius, iceMask))
         {
             speed = decreasedSpeed;
+            animationSpeedModifier = 0.5f;
         }
         else
         {
             speed = defaultSpeed;
+            animationSpeedModifier = 0.25f;
+        }
+    }
+
+    float detectDirection(float number)
+    {
+        if (number > 0)
+        {
+            return 1;
+        }
+        else if (number < 0)
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
         }
     }
 }
